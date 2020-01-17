0004581265faa1cb3ed60285d63dff19408f3b13
conflick0 <jpjhsu@gms.tku.edu.tw>
2020-01-17 19:23:44 +0800

    rename variable face_position with face_location

--- face_recognizer.py
+++ face_recognizer.py
@@ -16,7 +16,7 @@ class FaceRecognizer:
         self.model = load_model(MODEL_PATH)
         self.mtcnn_detector = MTCNN()
         self.inp_frame = None
-        self.face_positions = []
+        self.face_locations = []
         self.raw_faces = []
         self.inp_faces = []
         self.data_base_dict = {}
@@ -51,7 +51,7 @@ class FaceRecognizer:
     def __read_frame(self, inp_frame):
         # clear params when read new frame
         self.inp_frame = inp_frame
-        self.face_positions = []
+        self.face_locations = []
         self.raw_faces = []
         self.inp_faces = []
         self.face_dict = {}
@@ -60,19 +60,19 @@ class FaceRecognizer:
         faces = self.mtcnn_detector.detect_faces(self.inp_frame)
 
         for face in faces:
-            self.face_positions.append(face['box'])
+            self.face_locations.append(face['box'])
 
     def __face_extract(self):
         margin = 6
-        for face_pos in self.face_positions:
-            (x, y, w, h) = face_pos
+        for face_loc in self.face_locations:
+            (x, y, w, h) = face_loc
             face = self.inp_frame[y:y + h, x:x + w]
             face_margin = np.zeros((h + margin * 2, w + margin * 2, 3), dtype="uint8")
 
             try:
                 face_margin[margin:margin + h, margin:margin + w] = face
             except ValueError:
-                # Camera shaking cause detect error position, and lead to extract error face shape
+                # Camera shaking cause detect error location, and lead to extract error face shape
                 print('traceback __face_extract func')
                 print('{} : could not broadcast input array from shape {} into shape {}:'.format(ValueError, face.shape,
                                                                                                  face_margin.shape))
@@ -105,7 +105,7 @@ class FaceRecognizer:
                     min_dist = dist
                     face_ID = ID
 
-            self.face_dict[face_ID] = self.face_positions[i]
+            self.face_dict[face_ID] = self.face_locations[i]
 
     def __pre_whiten(self, x):
         if x.ndim == 4:
@@ -132,8 +132,8 @@ class FaceRecognizer:
         return output
 
     def __draw_face_box(self):
-        for (face_ID, face_pos) in self.face_dict.items():
-            (x, y, w, h) = face_pos
+        for (face_ID, face_loc) in self.face_dict.items():
+            (x, y, w, h) = face_loc
             cv2.rectangle(self.inp_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(self.inp_frame, face_ID, (x, y - 20), cv2.FONT_HERSHEY_DUPLEX,
                         1, (0, 255, 0), 1, cv2.LINE_AA)
